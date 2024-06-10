a0d05ef27e0f499b6184bc084385c57763908c92
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-10 13:00:48 +0200

    implemented ground movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerMovement : MonoBehaviour {
+    
+    private float minSpeed = 5.0f;
+    private float maxSpeed = 15.0f;
+    private float acceleration = 0.01f;
+    private float currSpeed = 5.0f;
+
+
+    private Vector3 movementVector;
+
+    void Update() {
+        if (movementVector != Vector3.zero) {
+            Move();
+        }
+        else {
+            StopMoving();
+        }
+        Debug.Log(currSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Move() {
+        transform.position += movementVector * currSpeed * Time.deltaTime;
+        StartCoroutine(Acceleration());
     }
+
+    void StopMoving() {
+        currSpeed = minSpeed;
+        StopCoroutine(Acceleration());
+    }
+
+    void OnMovement(InputValue value) {
+        Vector2 currInput = value.Get<Vector2>();
+        movementVector = new Vector3(currInput.x, 0, currInput.y);
+    }
+
+    IEnumerator Acceleration() {
+        while (currSpeed < maxSpeed) {
+            currSpeed += acceleration * Time.deltaTime;
+            yield return null;
+        }
+    }
+
 }
+
