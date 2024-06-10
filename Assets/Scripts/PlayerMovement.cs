dfdea1d7191009d40e4b4138b897fdb34fd60a43
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-10 14:03:50 +0200

    added jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
     
+    #region MovementParameters
     private float minSpeed = 5.0f;
     private float maxSpeed = 15.0f;
     private float acceleration = 0.01f;
-    private float currSpeed = 5.0f;
+    #endregion
+
+    #region JumpingParameters
+    private float jumpPower = 5.0f;
+
+    private float playerDistanceToGround = 1.0f;
+
+    #endregion
 
 
+    #region Variables
     private Vector3 movementVector;
+    private Vector3 airMovementVector;
+    private Rigidbody rb;
+    public bool grounded = false;
 
-    void Update() {
-        if (movementVector != Vector3.zero) {
-            Move();
-        }
-        else {
-            StopMoving();
-        }
-        Debug.Log(currSpeed);
+    private float currSpeed = 5.0f;
+    private float currAirSpeed = 5.0f;
+
+    #endregion
+
+    void Start() {
+        movementVector = Vector3.zero;
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(CheckForGround());
     }
 
+    void Update() {
+        if (grounded) {
+            if (movementVector != Vector3.zero) {
+                Move();
+            }
+            else {
+                StopMoving();
+            }
+        }
+        else {
+            AirMove();
+        }
+    }
+
+    
+    #region Movement
     void Move() {
         transform.position += movementVector * currSpeed * Time.deltaTime;
         StartCoroutine(Acceleration());
@@ -40,10 +69,54 @@ public class PlayerMovement : MonoBehaviour {
 
     IEnumerator Acceleration() {
         while (currSpeed < maxSpeed) {
-            currSpeed += acceleration * Time.deltaTime;
+            if (grounded) {    
+                currSpeed += acceleration * Time.deltaTime;
+            }
             yield return null;
         }
     }
+    #endregion
+
+
+    #region Jumping
+    void OnJump() {
+        if (!grounded) {
+            return;
+        }
+        UpdateAirMovement();
+        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+    }
+
+    void AirMove() {
+        if (grounded) {
+            transform.position += movementVector * currSpeed * Time.deltaTime;
+        }
+        else {
+            transform.position += airMovementVector * currAirSpeed * Time.deltaTime;
+        }
+    }
+
+    void UpdateAirMovement() {
+        airMovementVector = movementVector;
+        currAirSpeed = currSpeed;
+    }
+
+    IEnumerator CheckForGround() {
+        RaycastHit hit;
+
+        while (true) {
+            bool raycastSuccess = Physics.Raycast(transform.position, transform.up * -1, out hit);
+            if (raycastSuccess && hit.collider.gameObject.CompareTag("Ground") && hit.distance <= playerDistanceToGround) {
+                grounded = true;
+            }
+            else {
+                grounded = false;
+            }
+            yield return null;
+        }
+    }
+    #endregion
+    
 
 }
 
