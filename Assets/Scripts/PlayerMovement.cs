9fae4e7f9e3e4cdd598b31242d1f6c8015eb31cd
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-10 18:30:49 +0200

    added air movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,21 @@ public class PlayerMovement : MonoBehaviour {
     
     #region MovementParameters
     private float minSpeed = 5.0f;
-    private float maxSpeed = 15.0f;
-    private float acceleration = 0.01f;
+    private float maxSpeed = 12.0f;
+    private float acceleration = 0.1f;
     #endregion
 
+    #region AirMovementParameters
+    private float airMovementFactor = 0.25f;
+
+    private float airMovementScaling = 0.05f;
+    #endregion
+
+
     #region JumpingParameters
-    private float jumpPower = 5.0f;
+    private float jumpPower = 4.0f;
+
+    private float jumpScaling = 0.25f;
 
     private float playerDistanceToGround = 1.0f;
 
@@ -21,29 +30,36 @@ public class PlayerMovement : MonoBehaviour {
 
     #region Variables
     private Vector3 movementVector;
+
+    private Vector3 prevMovementVector;
+
     private Vector3 airMovementVector;
+
+    private Vector3 jumpMovementVector;
+
     private Rigidbody rb;
     public bool grounded = false;
 
-    private float currSpeed = 5.0f;
-    private float currAirSpeed = 5.0f;
+    private float currSpeed = 0.0f;
+
+    private float currAirSpeed = 0.0f;
+
+    private float currAirMovementScaling = 0.0f;
 
     #endregion
 
     void Start() {
+        currSpeed = minSpeed;
+        prevMovementVector = Vector3.zero;
         movementVector = Vector3.zero;
         rb = GetComponent<Rigidbody>();
         StartCoroutine(CheckForGround());
     }
 
-    void Update() {
+    void FixedUpdate() {
+        Debug.Log(currSpeed);
         if (grounded) {
-            if (movementVector != Vector3.zero) {
-                Move();
-            }
-            else {
-                StopMoving();
-            }
+            Move();
         }
         else {
             AirMove();
@@ -53,28 +69,45 @@ public class PlayerMovement : MonoBehaviour {
     
     #region Movement
     void Move() {
-        transform.position += movementVector * currSpeed * Time.deltaTime;
-        StartCoroutine(Acceleration());
+        if (grounded && movementVector != Vector3.zero) {
+            transform.position += movementVector * currSpeed * Time.deltaTime;
+            StartCoroutine(Acceleration());
+        }
     }
 
     void StopMoving() {
-        currSpeed = minSpeed;
         StopCoroutine(Acceleration());
+        currSpeed = minSpeed;
     }
 
     void OnMovement(InputValue value) {
         Vector2 currInput = value.Get<Vector2>();
+        prevMovementVector = movementVector;
         movementVector = new Vector3(currInput.x, 0, currInput.y);
+
+        if (movementVector == Vector3.zero || movementVector.x - prevMovementVector.x == 0 || movementVector.z - prevMovementVector.z == 0) {
+            StopMoving();
+        }
+
+        airMovementVector = movementVector * airMovementFactor;
     }
 
     IEnumerator Acceleration() {
         while (currSpeed < maxSpeed) {
-            if (grounded) {    
+            if (grounded) {
                 currSpeed += acceleration * Time.deltaTime;
             }
             yield return null;
         }
     }
+    #endregion
+
+    #region AirMovement
+
+    void AirMove() {
+        transform.position += (jumpMovementVector + (airMovementVector * (airMovementScaling * currAirSpeed))) * currAirSpeed * Time.deltaTime;
+    }
+
     #endregion
 
 
@@ -83,22 +116,15 @@ public class PlayerMovement : MonoBehaviour {
         if (!grounded) {
             return;
         }
-        UpdateAirMovement();
-        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        UpdateJumpVector();
+        float currJumpPower = jumpPower + (jumpScaling * (currSpeed - minSpeed));
+        rb.AddForce(Vector3.up * currJumpPower, ForceMode.Impulse);
     }
 
-    void AirMove() {
-        if (grounded) {
-            transform.position += movementVector * currSpeed * Time.deltaTime;
-        }
-        else {
-            transform.position += airMovementVector * currAirSpeed * Time.deltaTime;
-        }
-    }
-
-    void UpdateAirMovement() {
-        airMovementVector = movementVector;
+    void UpdateJumpVector() {
+        jumpMovementVector = movementVector;
         currAirSpeed = currSpeed;
+        currAirMovementScaling = currSpeed;
     }
 
     IEnumerator CheckForGround() {
