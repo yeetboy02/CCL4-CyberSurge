a57531f6b1334fcf70f1866a6409f9936fbf3c09
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-12 01:15:28 +0200

    added camera collisions

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -12,16 +12,26 @@ public class CameraMovement : MonoBehaviour {
 
     #region MovementParameters
     private Vector3 offset = new Vector3(0, 3.5f, -7);
+
+    private float minDistanceToPlayer = 1.0f;
+
     #endregion
+
 
     #region Variables
     [SerializeField] private Transform player;
 
-    private float distanceToPlayer;
+    private float baseDistanceToPlayer;
+
+    private float currDistanceToPlayer;
 
     private Vector3 currPlayerOffset;
 
+    private Vector3 prevPlayerOffset;
+
     private float fixedYOffset;
+
+    private bool colliding = false;
 
 
     #endregion
@@ -30,25 +40,55 @@ public class CameraMovement : MonoBehaviour {
         currPlayerOffset = offset;
         ApplyOffset();
         fixedYOffset = transform.position.y - player.position.y;
-        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        baseDistanceToPlayer = Vector3.Distance(transform.position, player.position);
+        currDistanceToPlayer = baseDistanceToPlayer;
+        prevPlayerOffset = currPlayerOffset;
         SetOffset();
     }
 
     void Update() {
+        SetZoom();
         ApplyOffset();
         LookAtPlayer();
     }
 
     #region Movement
     public void SetOffset() {
+        if (colliding) {
+            prevPlayerOffset = currPlayerOffset;
+        }
         currPlayerOffset = gameObject.transform.position - player.position;
         currPlayerOffset.Normalize();
-        currPlayerOffset *= distanceToPlayer;
+        currPlayerOffset *= currDistanceToPlayer;
         currPlayerOffset.y = fixedYOffset;
     }
 
-    public void ApplyOffset() {
+    void SetZoom() {
+        RaycastHit hit;
+        Physics.Raycast(player.position, currPlayerOffset, out hit, currDistanceToPlayer + 0.50001f);
+
+        if (hit.collider != null && hit.collider.gameObject != player.gameObject && hit.collider.gameObject != gameObject) {
+            Debug.Log(hit.collider);
+            if (hit.distance < minDistanceToPlayer) {
+                ResetOffset();
+            }
+            else {
+                currDistanceToPlayer = hit.distance;
+            }
+            SetOffset();
+        }
+        else {
+            currDistanceToPlayer = baseDistanceToPlayer;
+        }
+    }
+
+    void ApplyOffset() {
         gameObject.transform.position = player.position + currPlayerOffset;
+    }
+
+    void ResetOffset() {
+        currPlayerOffset = prevPlayerOffset;
+        currDistanceToPlayer = baseDistanceToPlayer;
     }
 
     #endregion
@@ -63,6 +103,19 @@ public class CameraMovement : MonoBehaviour {
         Vector2 input = value.Get<Vector2>();
         gameObject.transform.RotateAround(player.transform.position, Vector3.up, input.x * rotationSpeedX);
         SetOffset();
+    }
+
+    #endregion
+
+    #region Collisions
+
+    void OnTriggerStay(Collider other) {
+        SetZoom();
+        colliding = true;
+    }
+
+    void OnTriggerExit(Collider other) {
+        colliding = false;
     }
 
     #endregion
