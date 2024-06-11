6f2ccc244156ff2ad6673e2db01f736acf8c0942
Pramsi <manuelprammer@gmx.at>
2024-06-11 19:52:02 +0200

    creating the MainMenu

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+
+public void ChangeScene()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+}
