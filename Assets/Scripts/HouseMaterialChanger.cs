65594d8be854d81ea4fddb37d0a237a3241395cd
Pramsi <manuelprammer@gmx.at>
2024-06-19 12:40:55 +0200

    Added Materials and ColourChanging functionality for Houses

---
+++ Assets/Scripts/HouseMaterialChanger.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class HouseMaterialChanger : MonoBehaviour
+{
+    private int HouseStateNr;
+
+    [SerializeField]
+    private Material matRed;
+    [SerializeField]
+    private Material matBlue;
+    [SerializeField]
+    private Material matYellow;
+    //[SerializeField]
+    //private Material matPurple;
+    //[SerializeField]
+    //private Material matGreen;
+    //[SerializeField]
+    //private Material matOrange;
+
+
+    private void Start()
+    {
+        HouseStateNr = Random.Range(1, 3);
+
+        UpdatePotionAppearance();
+    }
+
+
+
+
+    private void UpdatePotionAppearance()
+    {
+        switch (HouseStateNr)
+        {
+            case 1:
+                this.gameObject.GetComponent<Renderer>().material = matRed;
+                break;
+            case 2:
+                this.gameObject.GetComponent<Renderer>().material = matBlue;
+                break;
+            case 3:
+                this.gameObject.GetComponent<Renderer>().material = matYellow;
+                break;
+            //case 4:
+            //    this.gameObject.GetComponent<Renderer>().material = matPurple;
+            //    break;
+            //case 5:
+            //    this.gameObject.GetComponent<Renderer>().material = matGreen;
+            //    break;
+            //case 6:
+            //    this.gameObject.GetComponent<Renderer>().material = matOrange;
+            //    break;
+        }
+    }
+}
